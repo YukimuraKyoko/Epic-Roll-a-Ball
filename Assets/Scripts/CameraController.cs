44276a3e64b40428d6863bf41ce844fd1661d791
Tina <TinaNguyen290@yahoo.com>
2020-04-17 23:12:05 -0700

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject target;
+    float desiredDistance = 4f;
+    float pitch = 0f;
+    float pitchMin = -40f;
+    float pitchMax = 60f;
+    float yaw = 0f;
+    float roll = 0f;
+    public float CameraHeight = 1f;
+    float cameraDistanceMax = 20f;
+    float cameraDistanceMin = 5f;
+    float cameraDistance = 10f;
+    public float scrollSpeed = 10f;
+
+
+    public float sensitivity = 15f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        pitch -= sensitivity * Input.GetAxis("Mouse Y");
+        yaw += sensitivity * Input.GetAxis("Mouse X");
+        pitch = Mathf.Clamp(pitch, pitchMin, pitchMax);
+
+
+
+        cameraDistance += Input.GetAxis("Mouse ScrollWheel") * scrollSpeed;
+        cameraDistance = Mathf.Clamp(cameraDistance, cameraDistanceMin, cameraDistanceMax);
+
+        transform.localEulerAngles = new Vector3(pitch, yaw, roll);
+        transform.position = target.transform.position - cameraDistance * transform.forward + Vector3.up * CameraHeight;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Rigidbody rb;
+    public float moveSpeed = 1000f;
+    Camera cam;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hdir = Input.GetAxisRaw("Horizontal");
+        float vdir = Input.GetAxisRaw("Vertical");
+
+
+        float cameraDirection = cam.transform.localEulerAngles.y;
+        Vector3 directionVector = new Vector3(hdir, 0, vdir);
+        Vector3 unitVector = directionVector.normalized;
+        Vector3 CamDir = unitVector;
+        CamDir = Quaternion.AngleAxis(cameraDirection, Vector3.up) * CamDir;
+
+        Vector3 forceVector = CamDir * moveSpeed * Time.deltaTime;
+        rb.AddForce(forceVector);
+    }
+}
